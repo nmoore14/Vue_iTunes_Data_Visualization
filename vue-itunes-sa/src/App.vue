747e1040a5751fbdfa35e5506ec2184f870e143b
Nick Moore <nickmoore_1@live.com>
2018-12-10 12:06:43 -0500

    trying to get the config setup

--- vue-itunes-sa/src/App.vue
+++ vue-itunes-sa/src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-<nav class="flex items-center justify-between flex-wrap bg-teal p-6">
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-    <div class="text-sm lg:flex-grow">
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-        Docs
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-        Examples
-      </a>
-      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
-        Blog
-      </a>
-    </div>
-    <div>
-      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Download</a>
-    </div>
-  </div>
-</nav>
+    <ul class="list-reset flex">
+      <li class="mr-6">
+        <a class="text-blue hover:text-blue-darker" href="#">Active</a>
+      </li>
+      <li class="mr-6">
+        <a class="text-blue hover:text-blue-darker" href="#">Link</a>
+      </li>
+      <li class="mr-6">
+        <a class="text-blue hover:text-blue-darker" href="#">Link</a>
+      </li>
+      <li class="mr-6">
+        <a class="text-grey-light cursor-not-allowed" href="#">Disabled</a>
+      </li>
+    </ul>
     <router-view/>
   </div>
 </template>
